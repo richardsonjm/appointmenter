b78eb8101c5d0beaeb3c099062b0aba4d99a2262
John Richardson <richardsonjm@gmail.com>
2016-07-15 13:17:34 -0400

    Add association between ailment and specialty

--- app/models/ailment.rb
+++ app/models/ailment.rb
@@ -1,6 +1,7 @@
 class Ailment < ActiveRecord::Base
   has_many :patients_ailments
   has_many :patients, through: :patients_ailments
+  belongs_to :specialty
 
   validates_presence_of :name
 end
--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,6 +1,7 @@
 class Specialty < ActiveRecord::Base
   has_many :doctors_specialties
   has_many :doctors, through: :doctors_specialties
+  has_many :ailments
 
   validates_presence_of :name
 end
