bfef27052ea5350c5d0d2939b386aa01ed71bbf8
John Richardson <richardsonjm@gmail.com>
2016-07-19 10:11:36 -0400

    Setup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Specialties
+orthopedics = FactoryGirl.create(:specialty, name: "Orthopedics")
+opthamology = FactoryGirl.create(:specialty, name: "Opthamology")
+cardiology = FactoryGirl.create(:specialty, name: "Cardiology")
+
+# Ailments
+broken_bones = FactoryGirl.create(:ailment, name: "broken bones", specialty: orthopedics)
+eye_trouble = FactoryGirl.create(:ailment, name: "eye trouble", specialty: opthamology)
+heart_disease = FactoryGirl.create(:ailment, name: "heart disease", specialty: cardiology)
+
+# Patients
+[broken_bones, eye_trouble, heart_disease].each do |ailment|
+  FactoryGirl.create(:patient, ailments: [ailment])
+end
+
+# Doctors
+[orthopedics, opthamology, cardiology].each do |specialty|
+  FactoryGirl.create(:doctor, specialties: [specialty])
+end
