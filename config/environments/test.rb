d59a0d475640b5b570ac876fab55a6848dff3f29
John Richardson <richardsonjm@gmail.com>
2016-07-20 10:26:03 -0400

    Add mailer for appointment updates

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Action mailer defaults
+  config.action_mailer.default_url_options = {host: 'example.com'}
+  ActionMailer::Base.default(from: 'test@example.com')
+
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
