99b5f94ce405d3157f42a5b6ef9fec79a57f74b0
John Richardson <richardsonjm@gmail.com>
2016-08-10 14:05:31 -0400

    fix indentation of validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   validates :email, presence: true, email: true
-   validates :first_name, presence: true
-   validates :last_name, presence: true
+  validates :email, presence: true, email: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   has_many :addresses, inverse_of: :user
 
